1f5fb7e28358e2ea1d0c36f4d04e59bd0c97baa8
Tikondra <tikondra47@gmail.com>
2020-04-29 17:28:38 +0400

    правки параметров в карточке

--- src/components/light-card.js
+++ src/components/light-card.js
@@ -2,24 +2,30 @@ import AbstractComponent from "./AbstractComponent";
 import {regexp} from "./utils";
 
 const createCard = (card) => {
-  const {title, price, image, id, vendor, available, country} = card;
+  const {parameters, ...rest} = card;
 
-  const getAvailable = (param) => param ? `В наличии` : `Нет в наличии`;
+  const getAvailable = (param) => {
+    if (param === `true`) {
+      return `В наличии`;
+    } else {
+      return `Нет в наличии`;
+    }
+  };
 
-  const getTitle = () => {
+  const getTitle = (title) => {
     return title.match(regexp).join(``);
   };
 
   return (
     `<li class="cards__item">
-        <a class="cards__link" href="card-light.html?${id}">
-            <img src=${image[0]} width="213" height="213" alt="${title}">
+        <a class="cards__link" href="card-light.html?${rest.id}">
+            <img src=${rest.image[0]} width="213" height="213" alt="${rest.title}">
         </a>
-        <h3 class="cards__title">${getTitle(title)}</h3>
-        <p class="cards__info">${getAvailable(available)}</p>
-        <p class="cards__info">Бренд: ${vendor}</p>
-        <p class="cards__info">Страна: ${country}</p>
-        <p class="cards__price">${price} ₽</p>
+        <h3 class="cards__title">${getTitle(rest.title)}</h3>
+        <p class="cards__info">${getAvailable(rest.available)}</p>
+        <p class="cards__info">${parameters[1]}</p>
+        <p class="cards__info">${parameters[2]}</p>
+        <p class="cards__price">${rest.price} ₽</p>
     </li>`
   );
 };
