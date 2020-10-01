8b122f46e69efac8962390d39107b7bba3bd0b47
Tikondra <tikondra47@gmail.com>
2020-10-01 13:04:08 +0400

    main refactoring

--- src/main.js
+++ src/main.js
@@ -1,18 +1,27 @@
-import {load, loadData, loadDataToFurniture, loadDataToChairs} from "./load/load";
-import {loadDataToLight} from "./load/load-light";
-import {loadDataToProduct} from "./load/load-card-page";
-import {loadDecorCard} from "./load/load-card";
-import {initBasket} from "./utils/basket";
-import {loadLightCardPage} from "./load/load-card-light-page";
-import {LOCAL_CHAIRS2, LOCAL_FURNITURE2} from "./utils/consts";
 import React from "react";
 import ReactDOM from "react-dom";
 import {Provider} from "react-redux";
 import {store} from "./store";
 import App from "./components/app/app.jsx";
 import {Operation as LightOperation} from "./reducer/light/data";
+import {loadData} from "./load/load";
+import {loadDecorCard} from "./load/load-card";
+import {loadLightCardPage} from "./load/load-card-light-page";
+import {loadFurnitureOffer, loadFurnitureOffers} from "./load/load-furniture";
 import {initSearch} from "./utils/search";
+import {initBasket} from "./utils/basket";
 
+// декор
+if (document.querySelector(`.decor-js`)) {
+  loadData();
+}
+if (document.querySelector(`.product`)) {
+  loadDecorCard();
+}
+// свет
+// if (document.querySelector(`.light-js`)) {
+//   loadDataToLight();
+// }
 if (document.querySelector(`#root`)) {
   store.dispatch(LightOperation.loadStartOffers());
   store.dispatch(LightOperation.loadFilters());
@@ -25,33 +34,20 @@ if (document.querySelector(`#root`)) {
       document.querySelector(`#root`)
   );
 }
-
-if (document.querySelector(`.decor-js`)) {
-  loadData();
-}
-if (document.querySelector(`.product`)) {
-  loadDecorCard();
-}
-
-if (document.querySelector(`.light-js`)) {
-  loadDataToLight();
-}
 if (document.querySelector(`.product-light`)) {
   loadLightCardPage();
 }
-
+// столы
 if (document.querySelector(`.furniture-js`)) {
-  load(loadDataToFurniture, LOCAL_FURNITURE2);
+  loadFurnitureOffers(1);
 }
-if (document.querySelector(`.product-furniture`)) {
-  load(loadDataToProduct, LOCAL_FURNITURE2, `furniture`, true);
-}
-
+// стулья
 if (document.querySelector(`.chairs-js`)) {
-  load(loadDataToChairs, LOCAL_CHAIRS2);
+  loadFurnitureOffers(2);
 }
-if (document.querySelector(`.product-chair`)) {
-  load(loadDataToProduct, LOCAL_CHAIRS2, `chair`, true);
+// карточка столов и стульев
+if (document.querySelector(`.product-furniture`)) {
+  loadFurnitureOffer();
 }
 
 initBasket();
